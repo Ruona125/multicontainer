9cf907a7ff3f714ddae3731ac22da802b74bb0bd
ruona <ogheneruonaagadagba4@gmail.com>
2023-04-27 13:10:28 +0100

    dockerized the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import MainComponent from "./MainComponent";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header className="header">
+        <div>This is a multicontainer application</div>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
       </header>
-    </div>
+      <div className="main">
+        <Routes>
+          <Route path="/" exact element={<MainComponent />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      everything is working
+      <Link to="/">Go back to home screen</Link>
+    </div>
+  );
+};
+export default OtherPage;
